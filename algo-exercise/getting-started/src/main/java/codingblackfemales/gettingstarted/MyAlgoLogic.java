84250bb7b477408fbe8353ce381693bb56aa0681
Jbkoala <jbailey0233@gmail.com>
2024-09-22 21:33:21 +0100

    I am creating a dynamic price based on spread (top book ask - top book buy) to position pricing close to market. Additionally have made intial comments of what I would like my algo to take into account when executating orders.

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -4,6 +4,8 @@ import codingblackfemales.action.Action;
 import codingblackfemales.action.NoAction;
 import codingblackfemales.algo.AlgoLogic;
 import codingblackfemales.sotw.SimpleAlgoState;
+import codingblackfemales.sotw.marketdata.AskLevel;
+import codingblackfemales.sotw.marketdata.BidLevel;
 import codingblackfemales.util.Util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +21,9 @@ public class MyAlgoLogic implements AlgoLogic {
 
         logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
 
+        final BidLevel nearTouch = state.getBidAt(0); // Highest buy price
+        AskLevel fTouch = state.getAskAt(0); // Lowest sell price
+        long price = (nearTouch.price - (fTouch.price - nearTouch.price)); // JB: 1) workout spread 2) Take away from the highest bid price
 
 
         /********
@@ -30,3 +35,9 @@ public class MyAlgoLogic implements AlgoLogic {
         return NoAction.NoAction;
     }
 }
+/*
+ ## Tips for my Passive Algo ##
+        - Need to add a condition to cancel existing order if price moves away from current market; Would need to set a range
+        - Want to make the price in range
+        - Would be ideal to calculate quantity to be similar to others in orderbook, to minimise market impact on pricing
+ */
