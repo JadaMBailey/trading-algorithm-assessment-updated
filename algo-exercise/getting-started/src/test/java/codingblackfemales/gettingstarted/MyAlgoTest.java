4dc43777aa9b69341350cf259a863c445d87c538
JadaMBailey <jbailey0233@gmail.com>
2024-11-05 23:57:06 +0000

    Have made further unit tests within this file.

--- algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
+++ algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
@@ -1,9 +1,22 @@
 package codingblackfemales.gettingstarted;
-
+import messages.order.Side;
+import codingblackfemales.action.Action;
+import codingblackfemales.action.NoAction;
 import codingblackfemales.algo.AlgoLogic;
-import org.junit.Test;
+import codingblackfemales.sotw.ChildOrder;
+import codingblackfemales.sotw.OrderState;
+import codingblackfemales.sotw.SimpleAlgoState;
+import codingblackfemales.sotw.marketdata.BidLevel;
+import messages.order.Side;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.mockito.Mockito;
 
 
 /**
@@ -18,50 +31,100 @@ import static org.junit.Assert.assertEquals;
  */
 public class MyAlgoTest extends AbstractAlgoTest {
 
+    private MyAlgoLogic myStretchLogic;
     @Override
     public AlgoLogic createAlgoLogic() {
         //this adds your algo logic to the container classes
-        return new MyStretchLogic();
+        myStretchLogic = new MyAlgoLogic();
+        return myStretchLogic;
     }
 
 
     @Test
     public void testDispatchThroughSequencer() throws Exception {
-        send(createTick8am());
-        send(createTick8_30am());
-        send(createTick8am());
-        send(createTick8am());
+
         //create a sample market data tick....
+        send(createTick8am()); //1
+        send(createTick8_30am()); //2
+        send(createTick9am()); //3
+        send(createTick9_30am()); //4
+        send(createTick10am()); //5
+        send(createTick10_30am()); //6
+        send(createTick11am()); //7
+        send(createTick11_30am()); //8
+        send(createTick12pm()); //9
+        send(createTick12_30pm()); //10
+        send(createTick1pm()); //11
+        send(createTick1_30pm()); //12
+        send(createTick2pm()); //13
+        send(createTick2_30pm()); //14
+        send(createTick3pm()); //15
+        send(createTick3_30pm()); //16
+        send(createTick4pm()); //17
 
-        //simple assert to check we had 3 orders created
-        assertEquals(1, container.getState().getChildOrders().size());
+
+        //simple assert to check we had a total of 3 orders (2 active and 1 cancelled)
+        assertEquals(3, container.getState().getChildOrders().size());
+    }
+
+
+    @Test
+    public void testOnly1SellOrder() throws Exception {
+        send(createTick8am()); //1
+        send(createTick8_30am()); //2
+        send(createTick9am()); //3
+        send(createTick9_30am()); //4
+        send(createTick10am()); //5
+        send(createTick10_30am()); //6
+        send(createTick11am()); //7
+        send(createTick11_30am()); //8
+        send(createTick12pm()); //9
+        send(createTick12_30pm()); //10
+        send(createTick1pm()); //11
+        send(createTick1_30pm()); //12
+        send(createTick2pm()); //13
+        send(createTick2_30pm()); //14
+        send(createTick3pm()); //15
+        send(createTick3_30pm()); //16
+        send(createTick4pm()); //17
+       
+        SimpleAlgoState state = container.getState();
+
+        long sellOrderCount = state.getActiveChildOrders().stream()
+                        .filter(order -> order.getSide() == Side.SELL)
+                        .count();
+
+        assertEquals(1, sellOrderCount);
     }
 
     @Test
-    public void checkVwapFunction() throws Exception{
-
-    }
-
-    @Test
-    public void checkOrderTracked() throws Exception{
-
-
-    }
-
-    @Test
-    public void checkthatAnOrderCanBeCreated() throws Exception{
+    public void shouldCreateOrder() throws Exception {
+        // Setup market conditions
         send(createTick8am());
-        send(createTick8_30am());
-        send(createTick8am());
-        send(createTick8am());
-        // container variable accessible
-        assertEquals(1,container.getState().getActiveChildOrders().size());
-    }
-
-    @Test
-    public void lowAskQuantity() throws Exception{
-        // I want to see that I am receiving the lowest quantity
+        send(createTick8_30am()); //2
+        send(createTick9am()); //3
+        send(createTick9_30am()); //4
+        send(createTick10am()); //5
+        send(createTick10_30am()); //6
+        send(createTick11am()); //7
+        send(createTick11_30am()); //8
+        send(createTick12pm()); //9
+        send(createTick12_30pm()); //10
+        send(createTick1pm()); //11
+        send(createTick1_30pm()); //12
+        send(createTick2pm()); //13
+        send(createTick2_30pm()); //14
+        send(createTick3pm()); //15
+        send(createTick3_30pm()); //16
+        send(createTick4pm()); //17
         
+        // // Mockito handles the creation of the mock object which is a ChildOrder
+        ChildOrder testOrder = Mockito.mock(ChildOrder.class);
+        container.getState().getActiveChildOrders().add(testOrder);
+        
+         // Assert that the order was added
+        //  assertTrue(container.getState().getActiveChildOrders().contains(testOrder));
     }
 
+    
 }
